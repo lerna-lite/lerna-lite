eece4eb948b21b274ede94c24eec81e4ca48d860
ghiscoding <gbeaulac@gmail.com>
2022-02-21 23:08:26 -0500

    tests(version): add more unit tests

---
+++ packages/version/src/lib/__tests__/git-tag.spec.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+import { exec } from '@lerna-lite/core';
+
+const { gitTag } = require("../git-tag");
+
+jest.mock('@lerna-lite/core', () => {
+  const { exec } = jest.requireActual('@lerna-lite/core')
+  return {
+    __esModule: true,
+    exec: jest.fn(exec)
+  }
+});
+
+describe("gitTag", () => {
+  (exec as any).mockResolvedValue();
+
+  it("creates an annotated git tag", async () => {
+    const tag = "v1.2.3";
+    const opts = { cwd: "default" };
+
+    await gitTag(tag, {}, opts);
+
+    expect(exec).toHaveBeenLastCalledWith("git", ["tag", tag, "-m", tag], opts, false);
+  });
+
+  it("signs the tag when configured", async () => {
+    const tag = "v3.2.1";
+    const opts = { cwd: "signed" };
+
+    await gitTag(tag, { signGitTag: true }, opts);
+
+    expect(exec).toHaveBeenLastCalledWith("git", ["tag", tag, "-m", tag, "--sign"], opts, false);
+  });
+
+  it("forces the tag when configured", async () => {
+    const tag = "v1.1.1";
+    const opts = { cwd: "forced" };
+
+    await gitTag(tag, { forceGitTag: true }, opts);
+
+    expect(exec).toHaveBeenLastCalledWith("git", ["tag", tag, "-m", tag, "--force"], opts, false);
+  });
+});
