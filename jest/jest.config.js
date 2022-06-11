a775b79e7a1018d5417d63e7e11065c7b4ab7a8a
ghiscoding <gbeaulac@gmail.com>
2022-06-10 22:23:41 -0400

    ci: disable jest coverage on windows platform & watch

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -5,55 +5,28 @@ module.exports = {
       diagnostics: false,
       isolatedModules: true,
       tsconfig: '<rootDir>/jest/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$'
+      stringifyContentPathRegex: '\\.html$',
     },
   },
   clearMocks: true,
-  collectCoverage: false,
-  collectCoverageFrom: [
-    'packages/**/*.+(js|ts)',
-    '!**/dist/**',
-    '!**/node_modules/**',
-    '!**/jest/**',
-  ],
+  collectCoverage: process.env.CI && process.env.LERNA_OS_TYPE !== 'windows',
+  collectCoverageFrom: ['packages/**/*.+(js|ts)', '!**/dist/**', '!**/node_modules/**', '!**/jest/**'],
   coverageDirectory: '<rootDir>/jest/jest-coverage',
-  coveragePathIgnorePatterns: [
-    '\\.d\\.ts$',
-    '<rootDir>/node_modules/'
-  ],
-  coverageReporters: [
-    'json',
-    'lcov',
-    'text',
-    'html'
-  ],
-  modulePathIgnorePatterns: ["/__fixtures__/"],
-  moduleFileExtensions: [
-    'json',
-    'js',
-    'ts'
-  ],
-  modulePaths: [
-    'src',
-    '<rootDir>/node_modules'
-  ],
+  coveragePathIgnorePatterns: ['\\.d\\.ts$', '<rootDir>/node_modules/'],
+  coverageReporters: ['json', 'lcov', 'text', 'html'],
+  modulePathIgnorePatterns: ['/__fixtures__/'],
+  moduleFileExtensions: ['json', 'js', 'ts'],
+  modulePaths: ['src', '<rootDir>/node_modules'],
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/helpers/silence-logging', '<rootDir>/helpers/set-npm-userconfig'],
   setupFilesAfterEnv: ['jest-extended/all', '<rootDir>/jest/setup-unit-test-timeout.js'],
   transform: {
-    '^.+\\.(ts|html)$': 'ts-jest'
+    '^.+\\.(ts|html)$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(@lerna-lite)/)'
-  ],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|js)',
-    '**/+(*.)+(spec|test).+(ts|js)'
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(@lerna-lite)/)'],
+  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/+(*.)+(spec|test).+(ts|js)'],
   testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   // verbose: !!process.env.CI,
   verbose: false,
 };
