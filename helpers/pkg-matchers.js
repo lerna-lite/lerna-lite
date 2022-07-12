f1fe3e2fda4ed4e73edea1782edd48a453208f39
ghiscoding <gbeaulac@gmail.com>
2022-07-12 00:12:35 -0400

    chore: use direct file import to improve code coverage

--- helpers/pkg-matchers.js
+++ helpers/pkg-matchers.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
-const { Package } = require('@lerna-lite/core');
+const { Package } = require('../packages/core/src/package');
 
 exports.toDependOn = createDependencyMatcher('dependencies');
 exports.toDevDependOn = createDependencyMatcher('devDependencies');
