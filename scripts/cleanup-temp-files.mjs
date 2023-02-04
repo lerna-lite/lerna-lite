a7ac7ca34688cce0f10b99f0c70ad9f1346b4614
ghiscoding <gbeaulac@gmail.com>
2023-02-04 12:52:17 -0500

    chore: only clean up folders after Jest exits

--- scripts/cleanup-temp-files.mjs
+++ scripts/cleanup-temp-files.mjs
@@ -5,6 +5,6 @@ import tempDir from 'temp-dir';
 
 glob(path.join(tempDir, '/lerna-*'), (error, deleteFolders) => {
   // delete silently all files/folders that startsWith "lerna-"
-  console.log(`Found ${deleteFolders.length} temp files/folders to cleanup.`);
+  console.log(`Found ${deleteFolders.length} temp folders to cleanup.`);
   (deleteFolders || []).forEach((folder) => fs.removeSync(folder));
 });
