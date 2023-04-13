cdafa3b1795130c77a475023d5546c35c0dbe3b4
ghiscoding <gbeaulac@gmail.com>
2023-04-13 12:35:13 -0400

    chore: skip some unit tests to find error that is throwing console error

--- packages/run/src/__tests__/run-command.spec.ts
+++ packages/run/src/__tests__/run-command.spec.ts
@@ -480,7 +480,7 @@ describe('RunCommand', () => {
       expect(collectedOutput).not.toContain('Nx read the output from the cache');
     });
 
-    it('should log a warning when using obsolete options with useNx', async () => {
+    it.skip('should log a warning when using obsolete options with useNx', async () => {
       collectedOutput = '';
 
       await lernaRun(testDir)('my-script', '--sort');
@@ -490,7 +490,7 @@ describe('RunCommand', () => {
       expect(collectedOutput).toContain('package-1');
     });
 
-    it('should log some infos when using "includeDependencies" options with useNx', async () => {
+    it.skip('should log some infos when using "includeDependencies" options with useNx', async () => {
       collectedOutput = '';
 
       await lernaRun(testDir)('my-script', '--include-dependencies', '--', '--silent');
