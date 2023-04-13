21fa6cdbf6167f9c1212e89ce8d7dd95874ccd43
ghiscoding <gbeaulac@gmail.com>
2023-04-13 00:59:28 -0400

    chore(deps): test useNx with lower version of Nx

--- packages/run/src/__tests__/run-command-with-nx-no-target-defaults.spec.ts
+++ packages/run/src/__tests__/run-command-with-nx-no-target-defaults.spec.ts
@@ -43,6 +43,8 @@ describe('RunCommand', () => {
     beforeAll(async () => {
       testDir = await initFixture('powered-by-nx');
       process.env.NX_WORKSPACE_ROOT_PATH = testDir;
+      process.env.NX_DAEMON = 'false';
+
       // @ts-ignore
       vi.spyOn(process, 'exit').mockImplementation(() => {
         // do nothing
