e29ce22374c48c5e1f97b0533363b4876d841c9f
Jason Dent <jason@streetsidesoftware.nl>
2023-04-18 17:52:12 +0200

    test: Add parsing test for version cli args

--- packages/cli/src/cli-commands/__tests__/cli-version-commands.spec.ts
+++ packages/cli/src/cli-commands/__tests__/cli-version-commands.spec.ts
@@ -2,6 +2,9 @@ import { describe, expect, it, vi } from 'vitest';
 
 vi.mock('@lerna-lite/version', () => null);
 import cliVersion from '../cli-version-commands';
+import yargs from 'yargs/yargs';
+
+const oc = expect.objectContaining;
 
 describe('Version Command CLI options', () => {
   it('should log a console error when versionCommand is not provided', async () => {
@@ -10,5 +13,29 @@ describe('Version Command CLI options', () => {
     await cliVersion.handler(undefined as any);
 
     expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('"@lerna-lite/version" is optional and was not found.'), expect.anything());
+  });
+
+  it.each`
+    args                                                                                   | expected
+    ${'version minor'}                                                                     | ${{ bump: 'minor' }}
+    ${'version prepatch'}                                                                  | ${{ bump: 'prepatch' }}
+    ${'version --dry-run'}                                                                 | ${{ 'dry-run': true, dryRun: true }}
+    ${'version --dry-run major'}                                                           | ${{ 'dry-run': true, dryRun: true, bump: 'major' }}
+    ${'version --conventional-commits --yes --conventional-prerelease --dry-run premajor'} | ${oc({ dryRun: true, bump: 'premajor', conventionalPrerelease: true })}
+    ${'version --dry-run true'}                                                            | ${{ dryRun: true, 'dry-run': true }}
+    ${'version --conventional-commits --yes --conventional-prerelease 0.2'}                | ${oc({ conventionalCommits: true, conventionalPrerelease: 0.2 })}
+    ${'version --conventional-commits --yes --conventional-prerelease=* premajor'}         | ${oc({ bump: 'premajor', conventionalCommits: true, conventionalPrerelease: '*' })}
+  `('should parse args as expected: "$args"', async ({ args, expected }) => {
+    const argv: string[] = typeof args === 'string' ? args.split(/\s+/g) : args;
+    const {
+      $0: _caller,
+      _,
+      ...options
+    } = await yargs()
+      .command(cliVersion as any)
+      .exitProcess(false)
+      .parse(argv);
+    // console.log('%o', options);
+    expect(options).toEqual(expected);
   });
 });
