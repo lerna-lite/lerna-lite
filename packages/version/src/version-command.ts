b4d9e1f45438436196891f471ddfafb25dced542
Ghislain B <gbeaulac@gmail.com>
2023-04-18 23:42:11 -0400

    fix(version): invalid --conventional-prerelease should throw, fix #569 (#577)
    * fix(version): invalid --conventional-prerelease should throw, fix #569

--- packages/version/src/version-command.ts
+++ packages/version/src/version-command.ts
@@ -477,7 +477,20 @@ export class VersionCommand extends Command<VersionCommandOption> {
       ? () => true
       : (_node, name) => prereleasePackageNames.has(name);
 
-    return collectPackages(this.packageGraph, { isCandidate }).map((pkg) => pkg?.name ?? '');
+    const prePkgs = collectPackages(this.packageGraph, { isCandidate }).map((pkg) => pkg?.name ?? '');
+
+    if (
+      this.options.conventionalPrerelease !== undefined &&
+      typeof this.options.conventionalPrerelease !== 'boolean' &&
+      prePkgs.length === 0
+    ) {
+      throw new ValidationError(
+        'ENOPRERELEASE',
+        `No packages found to prerelease when using "--conventional-prerelease ${this.options.conventionalPrerelease}".`
+      );
+    }
+
+    return prePkgs;
   }
 
   /**
