d940322515345d5fee44817f54652acdb28daea8
Ghislain B <gbeaulac@gmail.com>
2023-05-16 00:34:27 -0400

    Revert "chore: replace `glob` by `globby` to cleanup temp files (#605)" (#606)
    This reverts commit 56484ee5c6779be8a9417e752bfebe8e20b089bd.

--- scripts/cleanup-temp-files.mjs
+++ scripts/cleanup-temp-files.mjs
@@ -1,10 +1,10 @@
-import { globby } from 'globby';
+import { glob } from 'glob';
 import { join } from 'node:path';
 import { removeSync } from 'fs-extra/esm';
 import tempDir from 'temp-dir';
 import normalizePath from 'normalize-path';
 
-globby(normalizePath(join(tempDir, '/lerna-*'))).then((deleteFolders) => {
+glob(normalizePath(join(tempDir, '/lerna-*'))).then((deleteFolders) => {
   // delete silently all files/folders that startsWith "lerna-"
   console.log(`Found ${deleteFolders.length} temp folders to cleanup.`);
   (deleteFolders || []).forEach((folder) => removeSync(folder));
