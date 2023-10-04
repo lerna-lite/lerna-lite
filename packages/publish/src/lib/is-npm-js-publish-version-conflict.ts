137bd5511aef9d4161f20dac90325379ca274d98
Peter Somogyvari <peter.metz@unarin.com>
2023-10-04 10:25:19 -0700

    fix(publish): fix version conflict recognition for github packages
    The version conflict recognition logic was not working for github
    because it returns a different `code` for the same error compared to
    what npmjs.org returns.
    
    I've added two utility functions to check for the error code and
    message to make the code more readable in the error handler of the
    publish operation.
    
    This way there is a robust detection supporting both npmjs.com and
    npm.pkg.github.com registries, not just npmjs.org.
    
    Signed-off-by: Peter Somogyvari <peter.metz@unarin.com>

---
+++ packages/publish/src/lib/is-npm-js-publish-version-conflict.ts
@@ -0,0 +1,40 @@
+/**
+ * The purpose of this function is to determine whether the error object passed
+ * in represents a version conflict when publishing a package to the npm registry.
+ *
+ * An example of a version conflict error is:
+ * ```json
+ * {
+ *   "name": "HttpErrorGeneral",
+ *   "headers": {
+ *   },
+ *   "statusCode": 403,
+ *   "code": "E403",
+ *   "method": "PUT",
+ *   "uri": "https://registry.npmjs.org/@hyperledger%2fcactus-cmd-api-server",
+ *   "body": {
+ *       "success": false,
+ *       "error": "You cannot publish over the previously published versions: 2.0.0-alpha.2."
+ *   },
+ *   "pkgid": "@hyperledger/cactus-cmd-api-server@2.0.0-alpha.2",
+ *   "message": "403 Forbidden - PUT https://registry.npmjs.org/@hyperledger%2fcactus-cmd-api-server - You cannot publish over the previously published versions: 2.0.0-alpha.2."
+ * }
+ * ```
+ *
+ * @param ex The exception object to check for a version conflict error.
+ * @returns {boolean} true if the error represents a version conflict, false otherwise
+ */
+export function isNpmJsPublishVersionConflict(ex: unknown): boolean {
+  if (!ex || typeof ex !== 'object' || !(ex instanceof Error)) {
+    return false;
+  } else if ('code' in ex && ex.code === 'EPUBLISHCONFLICT') {
+    return true;
+  } else if (
+    'code' in ex &&
+    ex.code === 'E403' &&
+    ex.message?.includes('You cannot publish over the previously published versions')
+  ) {
+    return true;
+  }
+  return false;
+}
