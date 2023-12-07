5650624e2659b925c37eef11f118b1428f268c84
ghiscoding <gbeaulac@gmail.com>
2023-12-07 13:33:49 -0500

    chore: accept changelog with date of 1 or 2 digits to fix unit test on Node 21

--- helpers/serializers/serialize-changelog.ts
+++ helpers/serializers/serialize-changelog.ts
@@ -8,7 +8,7 @@ const serializeChangelog = {
       .serialize(normalizeNewline(str))
       .replace(/(\[.*?\])\(.*\/compare\/(.*?)\)/g, '$1(/compare/$2)')
       .replace(/(\[.*?\])\(.*\/commits\/GIT_HEAD\)/g, '$1(COMMIT_URL)')
-      .replace(/\(\d{4}-\d{2}-\d{2}\)/g, '(YYYY-MM-DD)');
+      .replace(/\(\d{4}-\d{1,2}-\d{1,2}\)/g, '(YYYY-MM-DD)');
   },
   test(val) {
     return val != null && typeof val === 'string';
