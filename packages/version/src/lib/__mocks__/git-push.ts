15d2e1c0abe63f42dc47eb23105deb49aee9d2d9
ghiscoding <gbeaulac@gmail.com>
2024-05-31 19:40:27 -0400

    chore: add git push tags one by one unit tests

--- packages/version/src/lib/__mocks__/git-push.ts
+++ packages/version/src/lib/__mocks__/git-push.ts
@@ -2,3 +2,6 @@ import { vi } from 'vitest';
 
 // to mock user modules, you _must_ call `vi.mock('./path/to/module')`
 export const gitPush = vi.fn(() => Promise.resolve());
+
+// to mock user modules, you _must_ call `vi.mock('./path/to/module')`
+export const gitPushSingleTag = vi.fn(() => Promise.resolve());
