0bd71473f3032a23e51f24d5924dfbc8f88f79b6
ghiscoding <gbeaulac@gmail.com>
2024-09-26 20:05:07 -0400

    chore: migrate node-glob code to globby

--- scripts/cleanup-temp-files.mjs
+++ scripts/cleanup-temp-files.mjs
@@ -1,11 +1,15 @@
-import { glob } from 'glob';
-import { join } from 'node:path';
+import { globby } from 'globby';
+import { join as pathJoin } from 'node:path';
 import { removeSync } from 'fs-extra/esm';
 import tempDir from 'temp-dir';
 import normalizePath from 'normalize-path';
 
-glob(normalizePath(join(tempDir, '/lerna-*'))).then((deleteFolders) => {
-  // delete silently all files/folders that startsWith "lerna-"
-  console.log(`Found ${deleteFolders.length} temp folders to cleanup.`);
-  (deleteFolders || []).forEach((folder) => removeSync(folder));
-});
+globby(normalizePath(pathJoin(tempDir, '/lerna-*')), { onlyDirectories: true })
+  .then((deleteFolders) => {
+    // silently delete all files/folders that startsWith "lerna-"
+    console.log(`Found ${deleteFolders.length} temp folders to cleanup.`);
+    (deleteFolders || []).forEach((folder) => removeSync(folder));
+  })
+  .catch((error) => {
+    console.error('Error occurred while cleaning up temp folders:', error);
+  });
