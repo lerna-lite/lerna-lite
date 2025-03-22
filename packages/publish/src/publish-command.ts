e759fbb94eeab9d4dd91a0837428948a0dffc89e
ghiscoding <gbeaulac@gmail.com>
2025-03-22 13:49:38 -0400

    fix(publish): replace +{SHA} with .{SHA} in lerna publish --canary

--- packages/publish/src/publish-command.ts
+++ packages/publish/src/publish-command.ts
@@ -472,7 +472,7 @@ export class PublishCommand extends Command<PublishCommandOption> {
 
       // semver.inc() starts a new prerelease at .0, git describe starts at .1
       // and build metadata is always ignored when comparing dependency ranges
-      return `${nextVersion}-${preid}.${Math.max(0, refCount - 1)}+${sha}`;
+      return `${nextVersion}-${preid}.${Math.max(0, refCount - 1)}.${sha}`;
     };
 
     if (isIndependent) {
