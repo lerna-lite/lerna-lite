8d00d2f5e71dc831ac5341597925146328c44304
Ghislain B. <gbeaulac@gmail.com>
2025-08-08 18:53:25 -0400

    fix(deps): replace `string-width` with `fast-string-width` (#1107)

--- packages/npmlog/src/gauge/template-item.ts
+++ packages/npmlog/src/gauge/template-item.ts
@@ -2,7 +2,7 @@
  * Inlined from deprecated package https://github.com/npm/gauge/blob/f8092518a47ac6a96027ae3ad97d0251ffe7643b
  */
 
-import stringWidth from 'string-width';
+import stringWidth from 'fast-string-width';
 
 function isPercent(num) {
   if (typeof num !== 'string') {
