199bfedbded089a6edb357047d56f46dda2547c0
Ghislain B. <gbeaulac@gmail.com>
2025-12-11 19:38:21 -0500

    fix(publish): shouldn't Comment on closed issues older than release (#1202)

--- packages/publish/src/lib/comment-resolved-items.ts
+++ packages/publish/src/lib/comment-resolved-items.ts
@@ -27,7 +27,7 @@ export async function remoteSearchBy(
 ) {
   const q =
     type === 'issue'
-      ? `repo:${owner}/${repo}+is:issue+state:closed+linked:pr+updated:>${startDate}`
+      ? `repo:${owner}/${repo}+is:issue+linked:pr+closed:>${startDate}`
       : `repo:${owner}/${repo}+type:pr+merged:>${startDate}`;
   logger.verbose('comments', `remote PR search query: ${q}`);
   return (await client.search!.issuesAndPullRequests({ q, advanced_search: true })).data.items;
@@ -51,22 +51,47 @@ export async function commentResolvedItems({
   const filterStartDate = previousTagLastCommit.commitDate;
 
   // closed linked issues and/or merged pull requests
-  let closedLinkedIssues: TypeNumberPair[] = [];
-  let mergedPullRequests: TypeNumberPair[] = [];
+  let closedLinkedIssues = new Set<TypeNumberPair>();
+  let mergedPullRequests = new Set<TypeNumberPair>();
 
   if (templates.issue) {
-    closedLinkedIssues = (await remoteSearchBy(client, 'issue', repo.owner, repo.name, filterStartDate, logger)).map((item) => ({
-      type: 'issue',
-      number: item.number,
-    }));
-    logger.verbose('comments', `Closed linked issues: ${closedLinkedIssues.map((c) => c.number).join(', ')}`);
+    const issues = await remoteSearchBy(client, 'issue', repo.owner, repo.name, filterStartDate, logger);
+    issues.forEach((item) => closedLinkedIssues.add({ type: 'issue', number: item.number }));
   }
 
+  if (templates.pullRequest) {
+    const pullRequests = (await remoteSearchBy(client, 'pr', repo.owner, repo.name, filterStartDate, logger)).filter((item) =>
+      commentFilterKeywords.some((startWord) => item.title.toLowerCase().startsWith(startWord.toLowerCase()))
+    );
+    pullRequests.forEach((item) => mergedPullRequests.add({ type: 'pr', number: item.number }));
+    logger.verbose(
+      'comments',
+      `Merged Pull Requests: ${Array.from(mergedPullRequests)
+        .map((c) => c.number)
+        .join(', ')}`
+    );
+
+    // issues might not be linked (fix keyword via description),
+    // but the fix keyword might be in the PR title so let's add those as issues if they're not yet included
+    if (templates.issue) {
+      pullRequests.forEach((p) => {
+        const match = p.title.match(/\b(fix|fixes)\s*#(\d+)/) || [];
+        if (match.length >= 2) {
+          const issueNumber = parseInt(match[2], 10);
+          closedLinkedIssues.add({ type: 'issue', number: issueNumber });
+        }
+      });
+    }
+  }
+
+  // issues could be added in 2 places, so let's log them here
   if (templates.issue) {
-    mergedPullRequests = (await remoteSearchBy(client, 'pr', repo.owner, repo.name, filterStartDate, logger))
-      .filter((item) => commentFilterKeywords.some((startWord) => item.title.toLowerCase().startsWith(startWord.toLowerCase())))
-      .map((item) => ({ type: 'pr', number: item.number }));
-    logger.verbose('comments', `Merged Pull Requests: ${mergedPullRequests.map((c) => c.number).join(', ')}`);
+    logger.verbose(
+      'comments',
+      `Closed linked issues: ${Array.from(closedLinkedIssues)
+        .map((c) => c.number)
+        .join(', ')}`
+    );
   }
 
   const repository = `${repo.owner}/${repo.name}`;
