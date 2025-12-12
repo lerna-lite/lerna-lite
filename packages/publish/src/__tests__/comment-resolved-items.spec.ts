a949a0e1716a6df0e4cb01106ba8cb295ebf2951
ghiscoding <gbeaulac@gmail.com>
2025-12-11 22:23:13 -0500

    chore: add missing mock

--- packages/publish/src/__tests__/comment-resolved-items.spec.ts
+++ packages/publish/src/__tests__/comment-resolved-items.spec.ts
@@ -18,15 +18,12 @@ vi.mock('@lerna-lite/npmlog', () => ({
   },
 }));
 
-// Mock parseGitRepo and getOldestCommitSinceLastTag
+// Mock parseGitRepo
 vi.mock('@lerna-lite/version', () => ({
   parseGitRepo: vi.fn().mockReturnValue({
     owner: 'owner',
     name: 'repo',
     host: 'github.com',
-  }),
-  getOldestCommitSinceLastTag: vi.fn().mockReturnValue({
-    commitDate: '2023-01-01',
   }),
 }));
 
@@ -192,15 +189,12 @@ describe('commentResolvedItems', () => {
     // Override dryRun to true
     mockOptions.dryRun = true;
 
-    // Mock parseGitRepo and getOldestCommitSinceLastTag
+    // Mock parseGitRepo
     vi.mock('@lerna-lite/version', () => ({
       parseGitRepo: vi.fn().mockReturnValue({
         owner: 'owner',
         name: 'repo',
         host: 'github.com',
-      }),
-      getOldestCommitSinceLastTag: vi.fn().mockReturnValue({
-        commitDate: '2023-01-01',
       }),
     }));
 
