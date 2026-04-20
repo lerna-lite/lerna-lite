96e89cc33269bd2e3d64bce6053f5df8ce301e87
ghiscoding <gbeaulac@gmail.com>
2026-04-20 16:54:52 -0400

    chore: fix test failures

--- packages/core/src/utils/nerf-dart.ts
+++ packages/core/src/utils/nerf-dart.ts
@@ -1,12 +1,13 @@
 // https://github.com/npm/npm/blob/0cc9d89/lib/config/nerf-dart.js
 // Use WHATWG URL API to normalize and strip sensitive parts
+// Normalize and strip sensitive parts from a registry URL, returning //host/path
 export function toNerfDart(uri) {
   const u = new URL(uri);
   u.username = '';
   u.password = '';
   u.search = '';
   u.hash = '';
-  let result = u.origin + u.pathname;
+  let result = '//' + u.host + u.pathname;
   if (!result.endsWith('/')) {
     result += '/';
   }
