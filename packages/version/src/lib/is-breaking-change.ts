76ef6f0413cbe9ab4e3257c0b2c061758cb7c0c3
ghiscoding <gbeaulac@gmail.com>
2026-09-18 17:39:50 -0400

    fix(deps): upgrade verkit to v0.5.0

--- packages/version/src/lib/is-breaking-change.ts
+++ packages/version/src/lib/is-breaking-change.ts
@@ -1,4 +1,4 @@
-import { difference, isLess } from 'verkit';
+import { difference, isLessThan } from 'verkit';
 
 export function isBreakingChange(currentVersion: string, nextVersion: string): boolean {
   const releaseType = difference(currentVersion, nextVersion);
@@ -9,10 +9,10 @@ export function isBreakingChange(currentVersion: string, nextVersion: string): b
     breaking = true;
   } else if (releaseType === 'minor') {
     // 0.1.9 => 0.2.0 is breaking
-    breaking = isLess(currentVersion, '1.0.0');
+    breaking = isLessThan(currentVersion, '1.0.0');
   } else if (releaseType === 'patch') {
     // 0.0.1 => 0.0.2 is breaking(?)
-    breaking = isLess(currentVersion, '0.1.0');
+    breaking = isLessThan(currentVersion, '0.1.0');
   } else {
     // versions are equal, or any prerelease
     breaking = false;
